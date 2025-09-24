36542a4ea6c5c20a057e22e30346ea19587bcab8
Ryftjet <victor.spans_5e@icloud.com>
2025-09-24 20:08:33 +1000

    Make app into class

--- pixelate.py
+++ pixelate.py
@@ -4,53 +4,73 @@ from PIL import Image, ImageTk
 from math import floor, sqrt
 from copy import deepcopy
 
-WIDTH = 500
+WIDTH = 300
 HEIGHT = 500
 
-root = Tk()
-root.geometry(f'{WIDTH}x{HEIGHT}')
 
+class PixelateApp:
+    def __init__(self):
+        # Set up window
+        self.root = Tk()
+        self.root.geometry(f'{WIDTH}x{HEIGHT}')
+        self.root.title('Pixelator')
 
-class Pixelate:
-    def __init__(self, root_):
-        self.root = root_
-        self.image = None
+        # Set up image
+        self.original_image = None
         self.pixelated_image = None
         self.image_to_show = None
 
-        self.open_button = Button(root_, text='Open', command=lambda: self.add_image())
-        self.open_button.place(x=WIDTH / 2 - 120, y=10)
-        self.pixel_rate_entry = Entry(root_)
-        self.pixel_rate_entry.place(x=WIDTH / 2 + 10, y=10)
-        self.pixelate_button = Button(root_, text='Pixelate',
-                                      command=lambda: self.decrease_pixel_quality(self.pixel_rate_entry.get()))
-        self.pixelate_button.place(x=WIDTH / 2 - 60, y=10)
-        self.size_label = Label(root_)
-        self.image_label = Label(root_)
-        self.image_label.place(x=0, y=100)
+        # Create button frame
+        self.button_frame = Frame(self.root)
+        self.button_frame.pack(side="top", fill="x")
 
-    def add_image(self):
+        # Create the open button
+        self.open_button = Button(
+            self.button_frame,
+            text='Open',
+            command=lambda: self.add_image()
+        )
+        self.open_button.pack(side="left")
+
+        # Create the pixel scale rate
+        self.pixel_scale_rate = Scale(
+            self.button_frame,
+            orient="horizontal",
+            from_=1,
+            to=1000,
+        )
+        self.pixel_scale_rate.pack(fill="x")
+        self.pixel_scale_rate.bind(
+            "<ButtonRelease>",
+            lambda scale_rate: self.decrease_pixel_quality(
+                int(self.pixel_scale_rate.get())
+            )
+        )
+
+        self.image_label = Label(self.root)
+        self.image_label.pack(side="bottom")
+        self.size_label = Label(self.root)
+        self.size_label.pack(side="bottom")
+
+    def add_image(self) -> None:
+        # Open the image file
         file_path = filedialog.askopenfilename()
-        c_image = Image.open(file_path)
-        size = int(c_image.size[0] / 5), int(c_image.size[1] / 5)
-        c_image = c_image.resize(size)
-        self.image = c_image
-        c_image = ImageTk.PhotoImage(c_image)
-        self.image_to_show = c_image
-        self.image_label.configure(image=self.image_to_show)
-        self.image_label.place(x=WIDTH / 2 - size[0] / 2, y=HEIGHT / 2 - size[1] / 2)
-        self.size_label.place(x=WIDTH / 2, y=HEIGHT / 2 + size[1] / 2 + 20, anchor=CENTER)
-        self.size_label.configure(text=f'{size[0]} x {size[1]}', justify=LEFT)
+        self.original_image = Image.open(file_path)
 
-    def decrease_pixel_quality(self, depth=64):
-        image_to_change = deepcopy(self.image)
-        if depth == '' or depth == '0':
-            depth = 64
-        else:
-            depth = int(depth)
+        self.image_to_show = ImageTk.PhotoImage(self.original_image)
+        # Disable inspections because self.image_to_show is technically not a Tkinter Image
+        # noinspection PyTypeChecker
+        self.image_label.configure(image=self.image_to_show)
+        self.size_label.configure(text=f'{self.original_image.size[0]} x {self.original_image.size[1]}', justify="left")
+
+    def decrease_pixel_quality(self, depth: int) -> None:
+        """Pixelate the image"""
+        image_to_change = deepcopy(self.original_image)
+
         size = image_to_change.size
         x_off = divmod(size[0], int(sqrt(depth)))
         y_off = divmod(size[1], int(sqrt(depth)))
+
         for x in range(0, size[0] - x_off[1], int(sqrt(depth))):
             for y in range(0, size[1] - y_off[1], int(sqrt(depth))):
                 list_of_pixels = []
@@ -68,13 +88,21 @@ class Pixelate:
                     b += pixel[2]
                 for i, j in list_of_pixels:
                     image_to_change.putpixel((i, j), (floor(r / depth), floor(g / depth), floor(b / depth)))
-        self.pixelated_image = ImageTk.PhotoImage(image_to_change)
+        reduced_image = image_to_change.convert('P', palette=Image.Palette.ADAPTIVE, colors=50)
+        self.pixelated_image = ImageTk.PhotoImage(reduced_image)
+        # Disable inspections because self.image_to_show is technically not a Tkinter Image
+        # noinspection PyTypeChecker
         self.image_label.configure(image=self.pixelated_image)
-        self.size_label.configure(text=f'{size[0] / int(sqrt(depth))} x {size[1] / int(sqrt(depth))}')
+        self.size_label.configure(text=f'{size[0] // int(sqrt(depth))} x {size[1] // int(sqrt(depth))}')
 
-    def loop(self):
+    def loop(self) -> None:
         self.root.mainloop()
 
 
-px = Pixelate(root)
-px.loop()
+def main():
+    app = PixelateApp()
+    app.loop()
+
+
+if __name__ == '__main__':
+    main()
